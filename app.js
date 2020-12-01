abb80965f8d5e99402f11287218dd8a8f5dc04aa
villivald <maxim.villivald@gmail.com>
2020-12-01 17:52:31 +0200

    add prettierrc

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send("Hello world!");
+app.get("/", (req, res) => {
+  res.send("Hello world!");
 });
 
 app.listen(port, () => console.log(`Example app listening on port  ${port}!`));
